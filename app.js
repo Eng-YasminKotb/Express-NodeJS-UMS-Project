d36460ccb09859d651c813d1139a09d364ea462d
Yasmin <Yasmin.Kotb21@gmail.com>
2024-05-08 19:31:00 +0300

    init

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express=require('express')
+const dbCon=require('./Server/config/db')
+
 const app=express()
 
 
@@ -6,6 +8,28 @@ const app=express()
 app.use(express.json())
 
 const port=process.env.PORT || 5000
+const db=mongoose.connection
+
+db.on('error',()=>{
+    console.log("Fail connection!")
+})
+
+
+db.once('open',()=>{
+    console.log("connected to DB Successful")
+})
+
+
+app.get('/',(req,res)=>{
+    res.send("hi user");
+    
+})
+
+
+
+
+
+
 
 app.listen(port,()=>{
     console.log(`App listening on port ${port}`);
